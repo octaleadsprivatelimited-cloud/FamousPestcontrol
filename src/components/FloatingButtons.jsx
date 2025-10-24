bab9e8c3fda3e9f7abd0c2644fb13ed6366c1bc8
Octaleads <octaleadsprivatelimited@gmail.com>
2025-10-24 11:52:12 +0000

    Update Hyderabad floating button image to hyderabad2.jpg

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -52,7 +52,7 @@ export default function FloatingButtons() {
 					<svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" className="map-icon">
 						<path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
 					</svg>
-					<img src="/images/hyderabad-location.webp" alt="Hyderabad" width="20" height="20" className="location-image" />
+					<img src="/images/hyderabad2.jpg" alt="Hyderabad" width="20" height="20" className="location-image" />
 				</div>
 				<div className="contact-btn-content">
 					<span className="contact-btn-title">Hyderabad</span>
